6a5ec3e12fb766f7e36c6d9099cc8e8709e5479e
Nicholas Lauritzen <nicholas.lauritzen@uchospitals.edu>
2019-10-22 08:14:58 -0500

    CSS Minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const twig = require('gulp-twig');
+const sass = require('gulp-sass');
 
 function Templates() {
     return gulp.src('src/*.html')
@@ -7,4 +8,14 @@ function Templates() {
             .pipe(gulp.dest('.'));
 }
 
+function Minify() {
+    return gulp.src(['C:/Users/wto6337/source/repos/GitHub/lindaprice/src/scss/*.scss'], {
+        allowEmpty: true,
+    }).pipe(sass({
+        outputStyle: 'compressed',
+        verbose: true,
+    })).pipe(gulp.dest('./css'));
+}
+
 exports.Templates = Templates;
+exports.Minify = Minify;
