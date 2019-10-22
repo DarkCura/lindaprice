e716fb8e6975baec8846494ea645ddbeda9e5b7d
Nicholas Lauritzen <nicholas.lauritzen@uchospitals.edu>
2019-10-22 08:03:04 -0500

    Moving Generated Page

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const twig = require('gulp-twig');
 function Templates() {
     return gulp.src('src/*.html')
             .pipe(twig())
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest('.'));
 }
 
 exports.Templates = Templates;
